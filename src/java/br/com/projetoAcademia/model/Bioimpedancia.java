0c82ca980e22b3cd29e06d52dc301772185fce40
massaiti01 <57904666+massaiti01@users.noreply.github.com>
2019-11-19 10:26:08 -0300

    correcao bioimpedancia

--- src/java/br/com/projetoAcademia/model/Bioimpedancia.java
+++ src/java/br/com/projetoAcademia/model/Bioimpedancia.java
@@ -16,156 +16,16 @@ public class Bioimpedancia {
     private Date data;
     private Aluno aluno;
     private Personal personal;
-    private Integer idade;
-    private Double altura;
-    private Double peso;
-    private Double triciptal;
-    private Double peitoral;
-    private Double subescapular;
-    private Double auxiliarMedio;
-    private Double suprailiaca;
-    private Double abdominal;
-    private Double coxa;
-    private Double biestiloide;
-    private Double bicondiliano;        
-    private Double indicePonderal;
-
-    public Integer getIdBioimpedancia() {
-        return idBioimpedancia;
-    }
-
-    public void setIdBioimpedancia(Integer idBioimpedancia) {
-        this.idBioimpedancia = idBioimpedancia;
-    }
-
-    public Date getData() {
-        return data;
-    }
-
-    public void setData(Date data) {
-        this.data = data;
-    }
-
-    public Aluno getAluno() {
-        return aluno;
-    }
-
-    public void setAluno(Aluno aluno) {
-        this.aluno = aluno;
-    }
-
-    public Personal getPersonal() {
-        return personal;
-    }
-
-    public void setPersonal(Personal personal) {
-        this.personal = personal;
-    }
-
-    public Integer getIdade() {
-        return idade;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
-
-    public Double getAltura() {
-        return altura;
-    }
-
-    public void setAltura(Double altura) {
-        this.altura = altura;
-    }
-
-    public Double getPeso() {
-        return peso;
-    }
-
-    public void setPeso(Double peso) {
-        this.peso = peso;
-    }
-
-    public Double getTriciptal() {
-        return triciptal;
-    }
-
-    public void setTriciptal(Double triciptal) {
-        this.triciptal = triciptal;
-    }
-
-    public Double getPeitoral() {
-        return peitoral;
-    }
-
-    public void setPeitoral(Double peitoral) {
-        this.peitoral = peitoral;
-    }
-
-    public Double getSubescapular() {
-        return subescapular;
-    }
-
-    public void setSubescapular(Double subescapular) {
-        this.subescapular = subescapular;
-    }
-
-    public Double getAuxiliarMedio() {
-        return auxiliarMedio;
-    }
-
-    public void setAuxiliarMedio(Double auxiliarMedio) {
-        this.auxiliarMedio = auxiliarMedio;
-    }
-
-    public Double getSuprailiaca() {
-        return suprailiaca;
-    }
-
-    public void setSuprailiaca(Double suprailiaca) {
-        this.suprailiaca = suprailiaca;
-    }
-
-    public Double getAbdominal() {
-        return abdominal;
-    }
-
-    public void setAbdominal(Double abdominal) {
-        this.abdominal = abdominal;
-    }
-
-    public Double getCoxa() {
-        return coxa;
-    }
-
-    public void setCoxa(Double coxa) {
-        this.coxa = coxa;
-    }
-
-    public Double getBiestiloide() {
-        return biestiloide;
-    }
-
-    public void setBiestiloide(Double biestiloide) {
-        this.biestiloide = biestiloide;
-    }
-
-    public Double getBicondiliano() {
-        return bicondiliano;
-    }
-
-    public void setBicondiliano(Double bicondiliano) {
-        this.bicondiliano = bicondiliano;
-    }
-
-    public Double getIndicePonderal() {
-        return indicePonderal;
-    }
-
-    public void setIndicePonderal(Double indicePonderal) {
-        this.indicePonderal = indicePonderal;
-    }
-    
-    
+    private Double gordura7Dobras;
+    private Double pesodeGordura7Dobras;
+    private Double massaMagra7Dobras;
+    private Double pesoResidual7Dobras;
+    private Double pesoOsseo7Dobras;
+    private Double pesoMuscular7Dobras;
+    private Double pesodeGordura3Dobras;
+    private Double massaMagra3Dobras;
+    private Double pesoResidual3Dobras;
+    private Double pesoOsseo3Dobras;
+    private Double pesoMuscular3Dobras;
     
 }
